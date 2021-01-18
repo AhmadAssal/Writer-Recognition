4e59990009c8120a422794c518fd2a67a01ab963
Samy <samyking.sb@gmail.com>
2021-01-18 20:05:02 +0200

    update order_writer_script

---
+++ order_writers_script.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+import random
+import os
+import shutil
+"""
+this script made by mad max aka "SamyBahaa"
+"""
+
+
+def read_forms_data():
+    """
+    this function read the forms.txt and extract the writer id and the form id that he wrote it.
+    :return: dictionary of writers and the form that he has  written  key= form_id and value = writer_id
+    """
+    "change the path of the forms.txt here :)"
+    path_forms_txt = '/home/ahmad/Desktop/AlexRun/Pattern Recognition/Dataset/ascii/forms.txt'
+    writers = {}
+    reader = open(path_forms_txt)
+    inputs = list(reader)
+    for line in inputs:
+        form_info = line.split(' ')
+        if "#" not in form_info[0]:
+            form_id = form_info[0]
+            writer_id = form_info[1]
+            writers[form_id] = writer_id
+
+    return writers
+
+
+def order_data_set():
+    """
+    this function call the read_forms_data() and collect all the forms written by the same author in one folder
+    :return:
+    """
+    "change the direction of old data set forms here :)"
+    old_path_data = '/home/ahmad/Desktop/AlexRun/Pattern Recognition/Dataset/forms/'
+    writers_set = read_forms_data()
+    for form in writers_set:
+        new_path_data = 'ordered forms/'
+        new_path_data += str(writers_set[form])
+        form_name = str(form) + '.png'
+        if not os.path.exists(new_path_data):
+            os.makedirs(new_path_data)
+            os.rename(old_path_data + form_name,
+                      new_path_data + '/' + form_name)
+        else:
+            os.rename(old_path_data + form_name,
+                      new_path_data + '/' + form_name)
+
+    return
+
+
+def filter():
+    suitable = []
+    path = "/home/ahmad/ordered forms"
+    for filename in os.listdir(path):
+        i = len(os.listdir(path + "/" + filename))
+        if (i >= 3):
+            suitable.append(filename)
+    suitable.sort()
+    #print(suitable)
+    return suitable,path
+
+
+def generate_random_test_cases(num):
+    new_path="/home/ahmad/Desktop/AlexRun/Pattern Recognition/Dataset/testcases/testcase_" + str(num) + "/"
+    os.makedirs(new_path)
+    suitable, dataset_path = filter()
+    random.seed(datetime.now())
+    random_choices = random.choices(suitable, k=3)
+    rand_index = random.randrange(0,2)
+
+        
+            
+
+
+
+
+
+
+
+
+# order_data_set()
+filter()
